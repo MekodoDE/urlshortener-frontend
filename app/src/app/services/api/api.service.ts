fa4188bde05cd9b708ae68f9262d597524e61db9
Marco <Marco@TOKYO>
2024-09-24 10:36:47 +0200

    Read out jwt token; Make functions observable

--- app/src/app/services/api/api.service.ts
+++ app/src/app/services/api/api.service.ts
@@ -1,60 +1,165 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { jwtDecode } from "jwt-decode";
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private config: any;
+  private configSubject: BehaviorSubject<any | null> = new BehaviorSubject<any | null>(null);
+  configUrl = '';
   urlKey = '';
 
   constructor(private http: HttpClient, private router: Router) {
     const urlParts = this.router.url.split('/');
-    // Remove the first element which is an empty string
-    urlParts.shift();
-    // Return the first segment of the URL (excluding the domain and protocol)
+    urlParts.shift(); // Remove the first element which is an empty string
     this.urlKey = urlParts[0];
+    this.configUrl = window.location.origin + environment.configUrl;
+    this.loadConfig();
   }
 
-  loadConfig() {
-    return this.http.get(environment.configUrl).toPromise().then((data) => {
-      this.config = data;
+  // Load config and cache it
+  loadConfig(): Observable<any> {
+    if (this.configSubject.value) {
+      // Return cached config as an observable
+      return of(this.configSubject.value);
+    }
+
+    // Fetch config from server and cache it
+    return this.http.get(this.configUrl).pipe(
+      tap((config) => this.configSubject.next(config)) // Cache the loaded config
+    );
+  }
+
+  // Helper function to ensure config is loaded before making an API call
+  private withConfig<T>(operation: (config: any) => Observable<T>): Observable<T> {
+    return this.loadConfig().pipe(
+      switchMap(config => operation(config))
+    );
+  }
+
+  appTitle(): Observable<any> {
+    return this.withConfig(config => {
+      return config.appTitle;
     });
   }
 
-  appTitle() {
-    return this.config?.appTitle;
+  getRedirect(): Observable<any> {
+    return this.withConfig(config => {
+      const url = `${config.apiUrl}/urls/${this.urlKey}`;
+      return this.http.get(url);
+    });
   }
 
-  getRedirect() {
-    const url = `${this.config?.apiUrl}${this.urlKey}`;
-    return this.http.get(url);
+  // Headers
+
+  getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('jwtToken');
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
   }
 
-  getUrls() {
-    const url = `${this.config?.apiUrl}`;
-    return this.http.get(url);
+  getUserId(): string | null {
+    const token = localStorage.getItem('jwtToken');
+    console.log(token)
+    if (token) {
+      const decoded: any = jwtDecode(token);
+      return decoded.sub;
+    }
+    return null;
   }
 
-  getUrl(urlKey: string) {
-    const url = `${this.config?.apiUrl}${urlKey}`;
-    return this.http.get(url);
+  getUserRole(): string | null {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      const decoded: any = jwtDecode(token);
+      return decoded.role;
+    }
+    return null;
   }
 
-  createUrl(newUrl: any) {
-    const url = `${this.config?.apiUrl}`;
-    return this.http.post(url, newUrl);
+  // User service
+  createUser(userData: any): Observable<any> {
+    return this.withConfig(config => {
+      const url = `${config.apiUrl}/users/`;
+      return this.http.get(url);
+    });
   }
 
-  updateUrl(urlKey: string, newUrl: any) {
-    const url = `${this.config?.apiUrl}${urlKey}`;
-    return this.http.put(url, newUrl);
+  getUsers(): Observable<any> {
+    return this.withConfig(config => {
+      const url = `${config.apiUrl}/users/`;
+      return this.http.get(url, { headers: this.getHeaders() });
+    });
   }
 
-  deleteUrl(urlKey: string) {
-    const url = `${this.config?.apiUrl}${urlKey}`;
-    return this.http.delete(url);
+  getUser(urlKey: string): Observable<any> {
+    return this.withConfig(config => {
+      const url = `${config.apiUrl}/users/${urlKey}`;
+      return this.http.get(url, { headers: this.getHeaders() });
+    });
+  }
+
+  updateUser(urlKey: string, userData: any): Observable<any> {
+    return this.withConfig(config => {
+      const url = `${config.apiUrl}/users/${urlKey}`;
+      return this.http.put(url, userData, { headers: this.getHeaders() });
+    });
+  }
+
+  deleteUser(urlKey: string): Observable<any> {
+    return this.withConfig(config => {
+      const url = `${config.apiUrl}/users/${urlKey}`;
+      return this.http.delete(url, { headers: this.getHeaders() });
+    });
+  }
+
+  login(loginData: any): Observable<any> {
+    return this.withConfig(config => {
+      const url = `${config.apiUrl}/users/login`;
+      return this.http.post(url, loginData);
+    });
+  }
+
+  // URL service with Observable
+  createUrl(newUrl: any): Observable<any> {
+    return this.withConfig(config => {
+      const url = `${config.apiUrl}/urls/`;
+      return this.http.post(url, newUrl, { headers: this.getHeaders() });
+    });
+  }
+
+  getUrls(): Observable<any> {
+    return this.withConfig(config => {
+      const url = `${config.apiUrl}/urls/`;
+      return this.http.get(url, { headers: this.getHeaders() });
+    });
+  }
+
+  getUrl(urlKey: string): Observable<any> {
+    return this.withConfig(config => {
+      const url = `${config.apiUrl}/urls/${urlKey}`;
+      return this.http.get(url, { headers: this.getHeaders() });
+    });
+  }
+
+  updateUrl(urlKey: string, newUrl: any): Observable<any> {
+    return this.withConfig(config => {
+      const url = `${config.apiUrl}/urls/${urlKey}`;
+      return this.http.put(url, newUrl, { headers: this.getHeaders() });
+    });
+  }
+
+  deleteUrl(urlKey: string): Observable<any> {
+    return this.withConfig(config => {
+      const url = `${config.apiUrl}/urls/${urlKey}`;
+      return this.http.delete(url, { headers: this.getHeaders() });
+    });
   }
 }
