c0aefd5f12aab11ccf64cdb1ef721621de2e3323
Marco <Marco@TOKYO>
2024-09-24 10:39:00 +0200

    Added login and register button

--- app/src/app/components/navbar/navbar.component.ts
+++ app/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  isLoggedIn: boolean = false;
 
+  constructor(private router: Router) {
+    // Check if the user is logged in by looking for the JWT token
+    this.isLoggedIn = !!localStorage.getItem('jwtToken');
+  }
+
+  logout() {
+    // Remove the token from localStorage and set isLoggedIn to false
+    localStorage.removeItem('jwtToken');
+    this.isLoggedIn = false;
+    this.router.navigate(['/']); // Redirect to home or login page
+  }
 }
