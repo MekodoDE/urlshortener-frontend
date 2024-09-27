d502c2e0aa6ae8a37faf429d22514eed4f1205fd
Marco <Marco@TOKYO>
2024-09-27 23:15:55 +0200

    Add activate button; Disable text fields if no admin

--- app/src/app/components/urls/url-edit/url-edit.component.ts
+++ app/src/app/components/urls/url-edit/url-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from '../../../services/api/api.service';
 
@@ -7,24 +7,28 @@ import { ApiService } from '../../../services/api/api.service';
   templateUrl: './url-edit.component.html',
   styleUrl: './url-edit.component.scss'
 })
-export class UrlEditComponent {
+export class UrlEditComponent implements OnInit {
   currentUrlKey: string = '';
   urlKey: string = '';
   redirectUrl: string = '';
+  isActive: boolean = true;
+  isAdmin: boolean = false;
 
-  constructor(private route: ActivatedRoute, private apiService: ApiService, private router: Router) { }
+  constructor(private apiService: ApiService, private route: ActivatedRoute, private router: Router) {
+    this.isAdmin = this.apiService.getUserRole() === 'admin';
+  }
 
-  ngOnInit() {
+  ngOnInit(): void {
     // Get URL key from route parameters
     this.route.params.subscribe(params => {
       const urlKey = params['urlKey'];
       if (urlKey) {
-        // Fetch URL details from API based on URL key
         this.currentUrlKey = urlKey;
         this.apiService.getUrl(urlKey).subscribe(
           (url: any) => {
             this.urlKey = url.url_key;
             this.redirectUrl = url.redirect_url;
+            this.isActive = url.is_active;
           },
           error => {
             console.error('Error fetching URL:', error);
@@ -34,16 +38,18 @@ export class UrlEditComponent {
     });
   }
 
-  onSubmit() {
+  onSubmit(): void {
     const updatedUrl = {
       url_key: this.urlKey,
-      redirect_url: this.redirectUrl
+      redirect_url: this.redirectUrl,
+      is_active: this.isActive
     };
 
+    // Call the update API
     this.apiService.updateUrl(this.currentUrlKey, updatedUrl).subscribe(
       () => {
         // Redirect to the URLs list after successful update
-        this.router.navigate(['/urls']);
+        this.router.navigate(['/url']);
       },
       (error) => {
         console.error('Error updating URL:', error);
