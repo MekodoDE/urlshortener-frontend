8149fe5b6fdff9822db09cd9c6604411067e6e39
Marco <Marco@TOKYO>
2024-10-06 22:44:38 +0200

    Check active status of url; Add error messages

--- app/src/app/components/redirect/redirect.component.ts
+++ app/src/app/components/redirect/redirect.component.ts
@@ -5,26 +5,41 @@ import { ApiService } from '../../services/api/api.service';
 @Component({
   selector: 'app-redirect',
   templateUrl: './redirect.component.html',
-  styleUrl: './redirect.component.scss'
+  styleUrls: ['./redirect.component.scss']
 })
 export class RedirectComponent {
-  response : any;
-  
+  response: any;
+  errorMessage: string = '';
+  isDeactivated: boolean = false;
+
   constructor(private httpService: ApiService, private router: Router) { }
 
   ngOnInit() {
     this.httpService.getRedirect().subscribe(
       (response) => {
         this.response = response;
+
+        // Handle deactivated link
+        if (this.response && !this.response.is_active) {
+          this.isDeactivated = true;
+          return; // Prevent further checks or redirection
+        }
+
+        // Handle valid and active link with a valid redirect URL
         if (this.response && this.response.redirect_url) {
-          // Change the entire URL to the redirect_url
           window.location.href = this.response.redirect_url;
         } else {
-          console.error('No redirect URL found in the response.');
+          // Handle cases where there's no redirect URL but response exists
+          this.errorMessage = 'No redirect URL found.';
         }
       },
       (error) => {
-        console.log(error);
+        // Handle cases where URL is not found or other errors occur
+        if (error.status === 404) {
+          this.errorMessage = 'The URL was not found.';
+        } else {
+          this.errorMessage = 'An unexpected error occurred. Please try again later.';
+        }
       }
     );
   }
