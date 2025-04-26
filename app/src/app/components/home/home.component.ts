cf4460adf830004f54935239146370ca511b25d7
Marco <Marco@TOKYO>
2025-04-26 15:10:40 +0200

    Add noindex tag

--- app/src/app/components/home/home.component.ts
+++ app/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../../services/api/api.service';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   standalone: false,
@@ -11,10 +12,15 @@ export class HomeComponent {
   appTitle = '';
 
   // Inject ApiService into the component's constructor
-  constructor(private apiService: ApiService) {}
+  constructor(
+    private apiService: ApiService,
+    private meta: Meta
+  ){}
 
   // Use ngOnInit lifecycle hook to call methods from the service
-  ngOnInit() {
+  ngOnInit(): void {
+    this.meta.updateTag({ name: 'robots', content: 'noindex' });
+
     this.appTitle = localStorage.getItem("appTitle") || "";
     if (this.appTitle == "") {
       this.apiService.configSubject.subscribe((val: any) => this.appTitle = val.appTitle)  
